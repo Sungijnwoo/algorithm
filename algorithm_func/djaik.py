937253b11d3a6480d143ee9e842c2b3c8cb3d1e3
wlsdn <tjdwnsdud3@naver.com>
2021-12-28 18:26:52 +0900

    add djaik func

---
+++ algorithm_func/djaik.py
@@ -0,0 +1,20 @@
+import heapq as hq
+
+def djaik(graph, pos):
+    distance = [1e9 for _ in range(len(graph))]
+    distance[pos] = 0
+    q = [(0, pos)]
+    hq.heapify(q)
+    while q:
+        dist, now = hq.heappop(q)
+        
+        if distance[now] < dist:
+            continue
+
+        for val, pos in graph[now]:
+            cost = dist + val
+            if cost < distance[pos]:
+                distance[pos] = cost
+                hq.heappush(q, (cost, pos))
+
+    return distance[1:]
