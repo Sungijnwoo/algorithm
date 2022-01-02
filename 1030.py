7017b2648872f234cf2e22f35530f8441ccc7a36
wlsdn <tjdwnsdud3@naver.com>
2022-01-02 16:23:21 +0900

    21.01.02

--- 1030.py
+++ 1030.py
@@ -2,27 +2,29 @@ s, n, k, r1, r2, c1, c2 = map(int, input().split())
 
 graph = [[0] * (n ** s) for _ in range(n**s)]
 
-def black(pos, time):
+def change_black(ny1, nx1, ny2, nx2):
+    for i in range(ny1, ny2):
+        for j in range(nx1, nx2):
+            graph[i][j] = 1
+
+def dfs(pos, time):
     if time == s:
         return
-    # print(pos)
     y1, x1, y2, x2 = pos
-    dy, dx = (y2-y1), (x2-x1)
-    black_y1, black_y2 = y1 + dy // n, y1 + ((dy // n) * (k+1))
-    black_x1, black_x2 = x1 + dx // n, x1 + ((dx // n) * (k+1))
-    for i in range(black_y1, black_y2):
-        for j in range(black_x1, black_x2):
-            graph[i][j] = 1
-    
+    dy, dx = (y2-y1) // n, (x2-x1) // n
+    black_range = [t for t in range((n - k) // 2, (n-k) // 2 + k)]
     for i in range(n):
-        for j in range(n):
-            # print(i*(dy//n), j*(dy//n), i*(dy//n)+(dy//n), j*(dy//n)+(dx//n))
-            black((i*(dy//n), j*(dy//n), i*(dy//n)+(dy//n), j*(dy//n)+(dx//n)), time+1)
+        for j in range(n): 
+            ny1 = y1 + dy * i
+            nx1 = x1 + dx * j
+            ny2 = ny1 + dy
+            nx2 = nx1 + dx
+            if i in black_range and j in black_range:
+                change_black(ny1, nx1, ny2, nx2)   
+            dfs((ny1, nx1, ny2, nx2), time+1)
 
 
-black((0, 0, len(graph), len(graph)), 0)
-# for i in graph:
-#     print(i)
+dfs((0, 0, len(graph), len(graph)), 0)
 for i in range(r1, r2+1):
     for j in range(c1, c2+1):
         print(graph[i][j], end="")
